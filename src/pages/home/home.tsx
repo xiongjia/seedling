8b79e41709db805187c7c0df6ab090aaf0ab60ce
xiongjia <lexiongjia@gmail.com>
2025-09-18 17:48:35 +0800

    fix svg issue

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -45,6 +45,18 @@ export const HomePage = () => {
                   <CardTitle className="text-sm font-medium">
                     Total Revenue
                   </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="text-muted-foreground h-4 w-4"
+                  >
+                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
                 </CardHeader>
                 <CardContent>
                   <div className="text-2xl font-bold">$45,231.89</div>
