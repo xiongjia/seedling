3c1c0bf638cad62588a1e29dabd4c390c4fa0f9d
xiongjia <lexiongjia@gmail.com>
2025-09-22 23:31:45 +0800

    updating home page]

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,8 +4,17 @@ import { LangSwitcher } from '@/components/layout/lang-switch'
 import { Main } from '@/components/layout/main'
 import { ThemeSwitcher } from '@/components/layout/theme-switch'
 import { TopNav } from '@/components/layout/top-nav'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { Tabs, TabsContent } from '@/components/ui/tabs'
-import {HomeCards } from './cards'
+import { HomeCards } from './cards'
+import { Overview } from './overview'
+import { RecentSales } from './recent-sales'
 
 export const HomePage = () => {
   const { t } = useTranslation()
@@ -41,6 +50,27 @@ export const HomePage = () => {
         >
           <TabsContent value="overview" className="space-y-4">
             <HomeCards />
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-7">
+              <Card className="col-span-1 lg:col-span-4">
+                <CardHeader>
+                  <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className="ps-2">
+                  <Overview />
+                </CardContent>
+              </Card>
+              <Card className="col-span-1 lg:col-span-3">
+                <CardHeader>
+                  <CardTitle>Recent Sales</CardTitle>
+                  <CardDescription>
+                    You made 265 sales this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <RecentSales />
+                </CardContent>
+              </Card>
+            </div>
           </TabsContent>
         </Tabs>
       </Main>
