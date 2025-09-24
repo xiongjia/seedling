c066d4216c65acb4587a1bdcb1c807f72c0bce75
xiongjia <lexiongjia@gmail.com>
2025-09-24 10:18:12 +0800

    fix react router basename issue

--- src/hooks/use-app-router.tsx
+++ src/hooks/use-app-router.tsx
@@ -4,15 +4,18 @@ import { HomePage } from '@/pages/home/home'
 import { SettingsPage } from '@/pages/settings'
 
 export const useAppRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Dashboard />,
-      children: [
-        { index: true, element: <HomePage /> },
-        { path: 'settings', element: <SettingsPage /> },
-      ],
-    },
-  ])
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Dashboard />,
+        children: [
+          { index: true, element: <HomePage /> },
+          { path: 'settings', element: <SettingsPage /> },
+        ],
+      },
+    ],
+    { basename: import.meta.env.VITE_APP_BASENAME || '/' },
+  )
   return router
 }
