d55b1c91bb3adc8f1c3117913895bd0330f19dcf
xiongjia <xiongjia@users.noreply.github.com>
2025-10-03 05:13:48 +0000

    deploy: 6b90b2f7314a117969c8299182ad5ff4137162dd

--- docs/assets/stories-BUA75qCc.js
+++ docs/assets/stories-BUA75qCc.js
@@ -1,4 +1,4 @@
-import{j as n}from"./iframe-BtGi-O7_.js";import{useMDXComponents as i}from"./index-D_Uu0UnC.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={a:"a",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(o.h2,{id:"storybook-for-seedling-ui-components",children:"Storybook for seedling UI components"}),`
+import{j as n}from"./iframe-DaS7K1gZ.js";import{useMDXComponents as i}from"./index-CPtR9ioL.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={a:"a",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(o.h2,{id:"storybook-for-seedling-ui-components",children:"Storybook for seedling UI components"}),`
 `,n.jsxs(o.p,{children:["This project utilizes Storybook v9 for its component documentation. The UI component library is built upon ",n.jsx(o.a,{href:"https://ui.shadcn.com/",rel:"nofollow",children:"Shadcn"}),"."]}),`
 `,n.jsxs(o.ul,{children:[`
 `,n.jsx(o.li,{children:"In the top toolbar, you'll find a light/dark theme switch and a multi-language switch."}),`
